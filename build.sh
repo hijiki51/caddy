2da108f5378eace487e4f51d305c59d8d82b6005
hijiki51 <hibiki0719euph@gmail.com>
2022-08-07 22:10:14 +0900

    🌱  init

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eux
+
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
+export GOOS=linux
+export GOARCH=amd64
+xcaddy build v2.5.2 \
+    --with github.com/caddy-dns/cloudflare
